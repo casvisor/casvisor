22ba702ac74ff718fa2641f8b745a57e1e591340
Yang Luo <hsluoyz@qq.com>
2017-05-01 12:42:03 +0800

    Add the hook.

--- main.go
+++ main.go
@@ -1,18 +1,40 @@
 package main
 
-import "github.com/astaxie/beego"
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
+)
+
 
 type Controller struct {
 	beego.Controller
 }
 
 func (c *Controller) Get() {
-	method := c.Ctx.Request.Method
-	url := c.Ctx.Request.RequestURI
-	c.Ctx.WriteString("method: " + method + ", path: " + url)
+	c.Ctx.WriteString("This is the content of the page.")
+}
+
+func (c *Controller) Post() {
+	c.Ctx.WriteString("This is the content of the page.")
+}
+
+func (c *Controller) Delete() {
+	c.Ctx.WriteString("This is the content of the page.")
+}
+
+func (c *Controller) Put() {
+	c.Ctx.WriteString("This is the content of the page.")
 }
 
 func main() {
+	var HasPermission = func(ctx *context.Context) {
+		method := ctx.Request.Method
+		url := ctx.Request.RequestURI
+		ctx.WriteString("Not authorized to access page, method: " + method + ", path: " + url)
+	}
+
+	beego.InsertFilter("*", beego.BeforeRouter, HasPermission)
+
 	beego.Router("*", &Controller{})
 	beego.Run()
 }
