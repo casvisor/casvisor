5f2f4254768b26188061c016890766710a00464c
Yang Luo <hsluoyz@qq.com>
2017-05-01 18:05:12 +0800

    Use ServeHTTP as the unit test method.

--- main_test.go
+++ main_test.go
@@ -1,26 +1,36 @@
 package main
 
 import (
-	"github.com/hsluoyz/casbin/api"
 	"testing"
+	"net/http"
+	"net/http/httptest"
+	"github.com/astaxie/beego"
+	"github.com/hsluoyz/beeauthz/authn"
+	"github.com/hsluoyz/beeauthz/authz"
 )
 
-func testEnforce(t *testing.T, e *api.Enforcer, sub string, obj string, act string, res bool) {
-	if e.Enforce(sub, obj, act) != res {
-		t.Errorf("%s, %s, %s: %t, supposed to be %t", sub, obj, act, !res, res)
+func testEnforce(t *testing.T, user string, path string, method string, code int) {
+	r, _ := http.NewRequest(method, path, nil)
+	r.SetBasicAuth(user,"123")
+	w := httptest.NewRecorder()
+	beego.BeeApp.Handlers.ServeHTTP(w, r)
+
+	if w.Code != code {
+		t.Errorf("%s, %s, %s: %d, supposed to be %d", user, path, method, w.Code, code)
 	}
 }
 
 func TestAuthzModel(t *testing.T) {
-	e := &api.Enforcer{}
-	e.InitWithFile("authz_model.conf", "authz_policy.csv")
+	beego.InsertFilter("*", beego.BeforeRouter,authn.NewAuthenticator("alice:123","bob:123"))
+	beego.InsertFilter("*", beego.BeforeRouter, authz.NewBasicAuthorizer())
+	beego.Router("*", &Controller{})
 
-	testEnforce(t, e, "alice", "/dataset1/resource1", "GET", true)
-	testEnforce(t, e, "alice", "/dataset1/resource1", "POST", true)
-	testEnforce(t, e, "alice", "/dataset1/resource2", "GET", true)
-	testEnforce(t, e, "alice", "/dataset1/resource2", "POST", false)
-	testEnforce(t, e, "bob", "/dataset2/resource1", "GET", false)
-	testEnforce(t, e, "bob", "/dataset2/resource1", "POST", true)
-	testEnforce(t, e, "bob", "/dataset2/resource2", "GET", true)
-	testEnforce(t, e, "bob", "/dataset2/resource2", "POST", true)
+	testEnforce(t, "alice", "/dataset1/resource1", "GET", 200)
+	testEnforce(t, "alice", "/dataset1/resource1", "POST", 200)
+	testEnforce(t, "alice", "/dataset1/resource2", "GET", 200)
+	testEnforce(t, "alice", "/dataset1/resource2", "POST", 403)
+	testEnforce(t, "bob", "/dataset2/resource1", "GET", 403)
+	testEnforce(t, "bob", "/dataset2/resource1", "POST", 200)
+	testEnforce(t, "bob", "/dataset2/resource2", "GET", 200)
+	testEnforce(t, "bob", "/dataset2/resource2", "POST", 200)
 }
