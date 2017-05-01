5a9857de73acd729aa0c214e70cc4bb18f6bb866
Yang Luo <hsluoyz@qq.com>
2017-05-01 13:09:02 +0800

    Get username via HTTP basic authentication.

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/context"
+	"github.com/astaxie/beego/plugins/auth"
+	"strings"
+	"net/http"
+	"encoding/base64"
 )
 
 
@@ -26,12 +30,34 @@ func (c *Controller) Put() {
 	c.Ctx.WriteString("This is the content of the page.")
 }
 
+func getUserName(r *http.Request) string {
+	s := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
+	if len(s) != 2 || s[0] != "Basic" {
+		return ""
+	}
+
+	b, err := base64.StdEncoding.DecodeString(s[1])
+	if err != nil {
+		return ""
+	}
+	pair := strings.SplitN(string(b), ":", 2)
+	if len(pair) != 2 {
+		return ""
+	}
+
+	return pair[0]
+}
+
 func main() {
 	var HasPermission = func(ctx *context.Context) {
+		user := getUserName(ctx.Request)
 		method := ctx.Request.Method
-		url := ctx.Request.RequestURI
-		ctx.WriteString("Not authorized to access page, method: " + method + ", path: " + url)
+		path := ctx.Request.RequestURI
+		ctx.WriteString("Not authorized to access page, user: " + user + ", method: " + method + ", path: " + path)
 	}
+
+	// authenticate every request
+	beego.InsertFilter("*", beego.BeforeRouter,auth.Basic("alice","123"))
 
 	beego.InsertFilter("*", beego.BeforeRouter, HasPermission)
 
