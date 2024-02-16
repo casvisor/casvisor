5965e49da21275c624dc79192a67efe870975ca5
magicwind <2814461814@qq.com>
2024-02-16 20:45:42 +0800

    feat: Add guacd in in ALLINONE image

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 if [ "${MYSQL_ROOT_PASSWORD}" = "" ] ;then MYSQL_ROOT_PASSWORD=123456 ;fi
 
-service mariadb start
+rc-service mariadb start
 
 mysqladmin -u root password ${MYSQL_ROOT_PASSWORD}
+
+/opt/guacamole/sbin/guacd -b 0.0.0.0 -L "$GUACD_LOG_LEVEL" -f
 
 exec /server --createDatabase=true
