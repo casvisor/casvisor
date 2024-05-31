3099eb9f609d8127145f18f1f268bd8775bf7d41
zhangyi <1142071946@qq.com>
2024-06-01 01:51:20 +0800

    feat: add swaggerItem to MenuItem

--- web/src/App.js
+++ web/src/App.js
@@ -208,6 +208,7 @@ class App extends Component {
     res.push(Setting.getItem(<Link to="/sessions">{i18next.t("general:Sessions")}</Link>, "sessions"));
     res.push(Setting.getItem(<Link to="/records">{i18next.t("general:Records")}</Link>, "records"));
     res.push(Setting.getItem(<Link to="/workbench" target="_blank">{i18next.t("general:Workbench")}</Link>, "workbench"));
+    res.push(Setting.getItem(<a target="_blank" rel="noreferrer" href={Setting.isLocalhost() ? `${Setting.ServerUrl}/swagger/` : "/swagger/"}>{i18next.t("general:Swagger")}</a>, "/swagger"));
 
     return res;
   }
