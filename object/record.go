b67f13af38ba371d9c880b3391fbdafb045cf584
Yaodong Yu <2814461814@qq.com>
2024-06-17 15:29:15 +0800

    feat: only log post request

--- object/record.go
+++ object/record.go
@@ -24,6 +24,12 @@ import (
 	"github.com/casvisor/casvisor/util"
 )
 
+var logPostOnly bool
+
+func init() {
+	logPostOnly = conf.GetConfigBool("logPostOnly")
+}
+
 type Record struct {
 	Id int `xorm:"int notnull pk autoincr" json:"id"`
 
@@ -163,6 +169,12 @@ func NewRecord(ctx *context.Context) (*Record, error) {
 }
 
 func AddRecord(record *Record) (bool, error) {
+	if logPostOnly {
+		if record.Method == "GET" {
+			return false, nil
+		}
+	}
+
 	record.Owner = record.Organization
 
 	affected, err := adapter.Engine.Insert(record)
