9b2af4f144ef01735f37bf325bf74f3ba251aef2
unknown <1102597040@qq.com>
2024-06-30 21:03:55 +0800

    feat: add "end-session" records to logs

--- controllers/tunnel.go
+++ controllers/tunnel.go
@@ -192,27 +192,7 @@ func (c *ApiController) GetAssetTunnel() {
 	for {
 		_, message, err := ws.ReadMessage()
 		if err != nil {
-			session := util.GlobalSessionManager.Get(sessionId)
-			if session != nil {
-				record, err := object.NewRecord(c.Ctx)
-				record.Action = "end-session"
-				record.Method = "POST"
-				record.RequestUri = "/api/end-session?id=" + sessionId
-				if err != nil {
-					logs.Info("Record message error: %s\n", err.Error())
-					return
-				}
-				userId := c.Ctx.Input.Params()["recordUserId"]
-				if userId != "" {
-					record.Organization, record.User = util.GetOwnerAndNameFromId(userId)
-				}
-				util.SafeGoroutine(func() {
-					_, err = object.AddRecord(record)
-					if err != nil {
-						panic(err)
-					}
-				})
-			}
+			c.CheckEndsessionAndRecord(sessionId)
 			_ = tunnel.Close()
 			err := object.CloseSession(sessionId, Normal, "Normal user exit")
 			if err != nil {
@@ -305,6 +285,30 @@ func (c *ApiController) TunnelMonitor() {
 			}
 			return
 		}
+	}
+}
+
+func (c *ApiController) CheckEndsessionAndRecord(sessionId string) {
+	session := util.GlobalSessionManager.Get(sessionId)
+	if session != nil {
+		record, err := object.NewRecord(c.Ctx)
+		record.Action = "end-session"
+		record.Method = "POST"
+		record.RequestUri = "/api/end-session?id=" + sessionId
+		if err != nil {
+			logs.Info("Record message error: %s\n", err.Error())
+			return
+		}
+		userId := c.Ctx.Input.Params()["recordUserId"]
+		if userId != "" {
+			record.Organization, record.User = util.GetOwnerAndNameFromId(userId)
+		}
+		util.SafeGoroutine(func() {
+			_, err = object.AddRecord(record)
+			if err != nil {
+				panic(err)
+			}
+		})
 	}
 }
 
